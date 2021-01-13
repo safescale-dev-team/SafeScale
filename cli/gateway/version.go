e94a6fb909523b2dac808e762bf50288ae06cfd6
Sébastien Besombes <sebastien.besombes@c-s.fr>
2021-01-13 11:04:37 +0100

    changed version

--- cli/gateway/version.go
+++ cli/gateway/version.go
@@ -2,6 +2,6 @@ package main
 // Build constants
 const (
     Version = "20.12.0-alpha"
-    BuildDate = "2021/01/13-11:00"
-    Revision = "d0bde62593525438f202e7ead2d479a14cf9020e"
+    BuildDate = "2021/01/13-11:01"
+    Revision = "237ff3a8c42bb7aa5c8e94762a03856d2e0ff4e4"
 )
