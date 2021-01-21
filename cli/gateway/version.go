2d8a7461ce045c62af01ed53184b8f7c4ae34aff
Sébastien Besombes <sebastien.besombes@c-s.fr>
2021-01-21 15:01:36 +0100

    Merge remote-tracking branch 'origin/develop' into feature/rest-gateway

--- cli/gateway/version.go
+++ cli/gateway/version.go
@@ -2,6 +2,6 @@ package main
 // Build constants
 const (
     Version = "20.12.0-alpha"
-    BuildDate = "2021/01/18-15:16"
-    Revision = "6c8a911091a78b7413ef73d9745317db3e81bd2c"
+    BuildDate = "2021/01/20-11:59"
+    Revision = "68795fd8f0619855c0ce8f4e9b4c91efad645ea8"
 )
