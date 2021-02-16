a1d99487baf7d21c93b98e4f585e3e50ca5ec7b0
Sébastien Besombes <sebastien.besombes@c-s.fr>
2021-02-16 15:38:08 +0100

    merge with develop branch

--- cli/gateway/version.go
+++ cli/gateway/version.go
@@ -2,6 +2,6 @@ package main
 // Build constants
 const (
     Version = "21.02.0-alpha"
-    BuildDate = "2021/02/15-17:59"
-    Revision = "8916def3a6121ef769926a32da5750ee2d8e9435"
+    BuildDate = "2021/02/16-10:24"
+    Revision = "97d76fe131b76201973bd65bf32d740bdf24db8c"
 )
